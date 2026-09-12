031126591ee3a2238e2136494802c982edc46006
Mauro Lavigna <maurolavigna88@gmail.com>
2026-09-12 14:42:51 +0200

    fix: admin middleware

--- backend/rotte/rotteEsami.js
+++ backend/rotte/rotteEsami.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ControllerEsame = require('../controllers/ControllerEsame'); // Il tuo controller con le logiche
 const authMiddleware = require('../controllers/authMiddleware'); // Il tuo middleware di autenticazione
+const adminMiddleware = require('../controllers/adminMiddleware');
 
 router.get('/ricerca-esame', ControllerEsame.ricercaEsame);
 
 router.post('/:idEsame/recensione', authMiddleware, ControllerEsame.aggiuntaRecensione);
 
-router.post('/aggiungi', authMiddleware, ControllerEsame.inserimentoEsame);
+router.post('/aggiungi', authMiddleware, adminMiddleware, ControllerEsame.inserimentoEsame);
 
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,25 @@ const rotteAutenticazione = require('./rotte/rotteAutenticazione');
 const rotteEsami = require('./rotte/rotteEsami'); 
 const app = express();
 const PORT = process.env.PORT;
+const http = require('http'); // importiamo il protocollo
+const httpServer = http.createServer(app); // dove lavorano express e websocket
+const {Server} = require('socket.io'); // importo la classe
 
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 app.use(cors({ 
   origin: process.env.FRONTEND_URL, // poi ci mettiamo il nostro url del sito 
   credentials: true,
 }));
 
+const websocket = new Server(httpServer, {
+  cors:{
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+  }
+});
+app.set(websocket)
 
 async function accendiServer() {
   const connessione = await mongoose.connect(process.env.MONGO_URI)
